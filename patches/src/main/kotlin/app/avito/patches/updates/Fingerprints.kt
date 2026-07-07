96f5dee6a57336c3a72b54d460964b240b696a96
xob0t <5348245@gmail.com>
2026-07-07 12:03:09 +0300

    fix: support Avito 228.0

--- patches/src/main/kotlin/app/avito/patches/updates/Fingerprints.kt
+++ patches/src/main/kotlin/app/avito/patches/updates/Fingerprints.kt
@@ -2,17 +2,21 @@ package app.avito.patches.updates
 
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.methodCall
+import app.morphe.patcher.string
 
 object ForceUpdateOpenFingerprint : Fingerprint(
     definingClass = "Lcom/avito/android/version_conflict/",
     returnType = "V",
-    parameters = listOf(
-        "Lcom/avito/android/forceupdate/screens/forceupdateroot/ForceUpdateRootOpenParams;",
-    ),
     filters = listOf(
+        string("open_params"),
         methodCall(
             definingClass = "Landroid/content/Context;",
             name = "startActivity",
         ),
     ),
+    custom = { method, _ ->
+        method.parameterTypes.singleOrNull()
+            ?.toString()
+            ?.startsWith("Lcom/avito/android/forceupdate/screens/forceupdateroot/") == true
+    },
 )
